3225c5f8faf545d169d654ad695b16ae61128690
therakib7 <therakib7@gmail.com>
2021-11-17 16:18:25 +0600

    scroll added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,27 @@
 /*==== Mobile menu =====*/
-$(document).ready(function(){
-  $(".toggle-btn").click(function(){
-     $("#side-menu").toggleClass("menu");
-   });
-});
+$(".exdda-toggle-btn").click(function(){
+   $("#exdda-side-menu").toggleClass("menu");
+   $(".exdda-toggle-btn").toggleClass("menu-btn");
+ });
 
 /*===== Tab ====*/
-$(document).ready(function () {
+$('#nav').children('li').first().children('a').addClass('active')
+    .next().addClass('is-open').show();
     
-    $('#nav').children('li').first().children('a').addClass('active')
-        .next().addClass('is-open').show();
-        
-    $('#nav').on('click', 'li > a', function() {
-        
-      if (!$(this).hasClass('active')) {
+$('#nav').on('click', 'li > a', function() {
+    
+  if (!$(this).hasClass('active')) {
 
-        $('#nav .is-open').removeClass('is-open').hide();
-        $(this).next().toggleClass('is-open').toggle();
-          
-        $('#nav').find('.active').removeClass('active');
-        $(this).addClass('active');
-      } else {
-        $('#nav .is-open').removeClass('is-open').hide();
-        $(this).removeClass('active');
-      }
-   });
+    $('#nav .is-open').removeClass('is-open').hide();
+    $(this).next().toggleClass('is-open').toggle();
+      
+    $('#nav').find('.active').removeClass('active');
+    $(this).addClass('active');
+  } else {
+    $('#nav .is-open').removeClass('is-open').hide();
+    $(this).removeClass('active');
+  }
 });
-
-
 
 /*===== Acrodion icon ====*/
 $(".rotate").click(function () {
